14121db932857de9c1fca40b08820c267a4c6ef1
Max Skybin <git@skybin.net>
2017-07-09 01:36:52 -0400

    Changed structure to OOP
    Also made the code more ideomatic Crystal in places

--- src/cdf.cr
+++ src/cdf.cr
@@ -2,51 +2,66 @@ require "./optparse"
 require "colorize"
 require "murmur3"
 
-module Dedup
+class CDF
+  alias FileList = Array(String)
+  alias Sizes = Hash(UInt64, Array(String))
+  alias Hashes = Hash(String, Array(String))
+
+  def initialize(file_list : FileList = FileList.new,
+                 options : OptParse::Options = OptParse::Options.new,
+                 hashes : Hashes = Hashes.new,
+                 start_time : Time = Time.now)
+    @file_list = file_list
+    @options = options
+    @hashes = hashes
+    @start_time = start_time
+  end
+
+  # Parses command line options
+  def get_options
+    @options = OptParse.parse_options @options
+  end
+
   # Returns file list matching specific pattern from filesystem
-  def Dedup.get_from_fs(options)
+  def get_files_from_fs
     print "\nStep 1/5: Getting the list of files               "
-    file_list = Array(String).new
-    path = options.root_dir + options.pattern
+    @file_list = FileList.new
+    path = @options.root_dir + @options.pattern
     begin
-      file_list = Dir[path]
+      @file_list = Dir[path]
     rescue
-      print_error_message "Fatal: Could not read '#{options.root_dir}'", true
+      print_error_message "Fatal: Could not read '#{@options.root_dir}'", true
     else
-      puts "#{file_list.size} total files and directores".colorize :green
+      puts "#{@file_list.size} total files and directores".colorize :green
     end
-    file_list
   end
 
   # Narrows down the list of possible duplicate candidates
   # by filtering out files based on file type and user-selected options
   # i.e hidden or zero-length files, directories, etc.
-  def Dedup.filter_by_file_type(file_list, options)
-    print "Step 2/5: Narrowing down the list (by options)    "
-    filtered_file_list = Array(String).new
-    file_list.each do |file|
+  def filter_by_type
+    print "Step 2/5: Narrowing down the list (by file type)  "
+    @file_list.reject! do |file|
       begin
-        file_stats = File.stat(file)
-        if file_stats.file? && !file_stats.symlink? &&
-           (file_stats.size != 0 || options.zero_len) &&
-           (File.basename(file)[0] != '.' || options.hidden)
-          filtered_file_list << file
-        end
+        delete_file = !File.file?(file) || File.symlink?(file) ||
+                      (File.size(file) == 0 && !@options.zero_len) ||
+                      (File.basename(file)[0] == '.' && !@options.hidden)
       rescue
-        # silently ignore files we could not get sizes for
+        true # stop processing files we could not stat
+      else
+        delete_file
       end
     end
-    puts "#{filtered_file_list.size} candidates found".colorize :green
-    filtered_file_list
+
+    puts "#{@file_list.size} candidates found".colorize :green
   end
 
   # Narrows down the list of possible duplicate candidates
   # by filtering out files with unque sizes
-  def Dedup.filter_by_size(file_list)
+  def filter_by_size
     print "Step 3/5: Narrowing down the list (by size)       "
-    filtered_file_list = Array(String).new
-    sizes = {} of UInt64 => Array(String)
-    file_list.each do |file|
+    sizes = Sizes.new
+    @file_list.each do |file|
       begin
         size = File.size file
       rescue
@@ -59,23 +74,17 @@ module Dedup
         end
       end
     end
+    @file_list.clear
     sizes.each do |size, files|
-      if files.size > 1
-        files.each do |file|
-          filtered_file_list << file
-        end
-      end
+      @file_list += files if files.size > 1
     end
-    puts "#{filtered_file_list.size} candidates found".colorize :green
-    filtered_file_list
+    puts "#{@file_list.size} candidates found".colorize :green
   end
 
   # Hash all files in the file_list and return a Hash with duplicates only
-  def Dedup.find_dups_by_hash(file_list)
+  def find_dups_by_hash
     print "Step 4/5: Identifying duplicates (may take a bit) "
-    all_hashes = {} of String => Array(String)
-    duplicate_hashes = {} of String => Array(String)
-    file_list.each do |file|
+    @file_list.each do |file|
       begin
         # TODO: Find more efficient way to do this for large files
         buffer = File.new file
@@ -84,35 +93,26 @@ module Dedup
       rescue
         # silently ignore files we could not hash
       else
-        if all_hashes.has_key? hash
-          all_hashes[hash] += [file]
+        if @hashes.has_key? hash
+          @hashes[hash] += [file]
         else
-          all_hashes[hash] = [file]
+          @hashes[hash] = [file]
         end
       end
     end
-    all_hashes.each do |hash, files|
-      if files.size > 1
-        files.each do |file|
-          if duplicate_hashes.has_key? hash
-            duplicate_hashes[hash] += [file]
-          else
-            duplicate_hashes[hash] = [file]
-          end
-        end
-      end
+    @hashes.each do |hash, files|
+      @hashes.delete(hash) if files.size < 2
     end
-    dup_groups, dup_files = get_summary duplicate_hashes
+    dup_groups, dup_files = get_summary
     puts "#{dup_files} duplicates found".colorize :green
-    duplicate_hashes
   end
 
   # Writes file names of duplicate files to an output file
-  def Dedup.write_to_file(hashes, options)
+  def write_to_file
     print "Step 5/5: Writing results to output file          "
     buffer = String::Builder.new
     buffer << "Duplicate files\n"
-    hashes.each do |hash, files|
+    @hashes.each do |hash, files|
       buffer << "Duplicate Group (hash = #{hash}):\n"
       files.each do |file|
         buffer << "   ==> #{file}\n"
@@ -121,58 +121,51 @@ module Dedup
     end
 
     begin
-      out_file = File.open options.out_file, "w"
-      File.write options.out_file, buffer.to_s
+      out_file = File.open @options.out_file, "w"
+      File.write @options.out_file, buffer.to_s
       out_file.close
     rescue
-      print_error_message "Fatal: Could not write to file '#{options.out_file}'", true
+      print_error_message "Fatal: Could not write to file '#{@options.out_file}'", true
     else
-      puts "saved to '#{options.out_file}'".colorize :green
+      puts "saved to '#{@options.out_file}'".colorize :green
     end
   end
 
   # Helper function. Returns summary of file analysis
-  def Dedup.get_summary(hashes)
+  def get_summary
     dup_groups = 0
     dup_files = 0
-    hashes.each do |hash, files|
+    @hashes.each do |hash, files|
       dup_groups += 1
       dup_files += files.size
     end
     [dup_groups, dup_files]
   end
 
-  # Helper function: Prints error message
-  # and optionally exits the program
-  def Dedup.print_error_message(message, exit?)
+  # Prints analysis summary on-screen
+  def print_summary
+    dup_groups, dup_files = get_summary
+    elapsed_time = Time.now - @start_time
+    puts "\nSummary".colorize :green
+    puts "Duplicate groups : #{dup_groups}"
+    puts "Duplicate files  : #{dup_files}"
+    printf "Analysis completed in %02dm %02ds\n", elapsed_time.minutes, elapsed_time.seconds
+  end
+
+  # Helper function: Prints error message and optionally exits the program
+  def print_error_message(message, exit?)
     puts message.colorize :red
     exit 1 if exit?
   end
-
-  # ### MAIN PROGRAM ###
-
-  # Get command line options
-  options = OptParse::Options.new
-  options = OptParse.parse_options options
-
-  # Track time
-  time = Time.now
-
-  # Main processing logic
-  file_list = get_from_fs options
-  file_list = filter_by_file_type file_list, options
-  file_list = filter_by_size file_list
-  hashes = find_dups_by_hash file_list
-  write_to_file hashes, options
-
-  # Print summary
-  dup_groups, dup_files = get_summary hashes
-  time = Time.now - time
-  puts "\nSummary".colorize :green
-  puts "Duplicate groups : #{dup_groups}"
-  puts "Duplicate files  : #{dup_files}"
-  printf "Analysis completed in %02dm %02ds\n", time.minutes, time.seconds
-
-  # ### END MAIN PROGRAM ###
-
 end
+
+# ### MAIN PROGRAM ###
+cdf = CDF.new
+cdf.get_options
+cdf.get_files_from_fs
+cdf.filter_by_type
+cdf.filter_by_size
+cdf.find_dups_by_hash
+cdf.write_to_file
+cdf.print_summary
+# ### END MAIN PROGRAM ###
